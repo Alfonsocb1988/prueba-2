25e01ac2c6e730c4fd3d1d04437189283eb57e97
Alfonsocb1988 <78821686+Alfonsocb1988@users.noreply.github.com>
2021-02-09 21:20:45 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,71 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-whatsapp-image-2021-02-09-at-114130",
+      "name": "WhatsApp Image 2021-02-09 at 11.41.30",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 384,
+      "initialViewParameters": {
+        "yaw": -1.596824033393581,
+        "pitch": 0.13972198013847859,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8537645909780753,
+          "pitch": 0.11632420676385813,
+          "rotation": 0,
+          "target": "1-whatsapp-image-2021-02-09-at-114725"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.09592417006974507,
+          "pitch": 0.07219470919951831,
+          "title": "Biblioteca",
+          "text": "Contamos con una biblioteca totalmente actualizada, con los últimos títulos en literatura infantil."
+        }
+      ]
+    },
+    {
+      "id": "1-whatsapp-image-2021-02-09-at-114725",
+      "name": "WhatsApp Image 2021-02-09 at 11.47.25",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 384,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
